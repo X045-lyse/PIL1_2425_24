8216f7db1aa9cdbc6fe519b0d7cb85145fd22055
Mapinx <maxwekenon@gmail.com>
2025-06-09 12:41:01 +0100

    Activation et vérification du fonctionnement du serveur flask

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,7 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
 auth_bp = Blueprint('auth', __name__)
 
-@auth_bp.route('/register', methods=['GET'])
+@auth_bp.route('/register', methods=['POST'])
 def register():
-    return {"message": "Bienvenue sur /register"}
+    data = request.get_json()
+
+    nom = data.get('nom')
+    email = data.get('email')
+    mot_de_passe = data.get('mot_de_passe')
+    role = data.get('role')
+
+    # Vérifier que les données sont présentes
+    if not all([nom, email, mot_de_passe, role]):
+        return jsonify({"error": "Champs manquants"}), 400
+
+    # Pour l'instant, on affiche juste ce qu'on a reçu
+    return jsonify({
+        "message": "Utilisateur reçu !",
+        "utilisateur": {
+            "nom": nom,
+            "email": email,
+            "role": role
+        }
+    }), 201
