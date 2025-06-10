6ce2c106c023f128949e8e1a102fc5c885243464
Mapinx <maxwekenon@gmail.com>
2025-06-10 17:29:27 +0100

    Ajout des routes

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from routes.auth import auth_bp  # 👈 importer ton blueprint
 from models import db
+from routes.trajets import trajets_bp
+from routes.demandes import demandes_bp
+from routes.messaging import messaging_bp
+from routes.matching import matching_bp
 
 app = Flask(__name__)
 
@@ -13,6 +17,10 @@ db.init_app(app)
 
 # 👇 ENREGISTRER LE BLUEPRINT avec le préfixe /api
 app.register_blueprint(auth_bp, url_prefix='/api')
+app.register_blueprint(trajets_bp, url_prefix='/api')
+app.register_blueprint(demandes_bp,url_prefix='/api')
+app.register_blueprint(messaging_bp,url_prefix='/api')
+app.register_blueprint(matching_bp,url_prefix='/api')
 
 @app.route('/')
 def home():
