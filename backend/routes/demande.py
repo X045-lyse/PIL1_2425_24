3e5ad267cf9d0c6583ed1cd35d74c3aaf6006147
Mapinx <maxwekenon@gmail.com>
2025-06-17 17:35:43 +0100

    Ajustement de l'apparence

--- backend/routes/demande.py
+++ backend/routes/demande.py
@@ -115,37 +115,3 @@ def get_trajet(id):
         "arrivee_lat": trajet.arrivee_lat,
         "arrivee_lng": trajet.arrivee_lng
     })
-
-
-
-
-@demande_bp.route('/<int:id>', methods=['GET'])
-def get_demande(id):
-    demande = Demande.query.get_or_404(id)
-    return jsonify({
-        "id": demande.id,
-        "point_depart": demande.point_depart,
-        "point_arrivee": demande.point_arrivee,
-        "date": demande.date.isoformat(),
-        "heure_souhaitee": demande.heure_souhaitee.strftime("%H:%M"),
-        "depart_lat": demande.depart_lat,
-        "depart_lng": demande.depart_lng,
-        "arrivee_lat": demande.arrivee_lat,
-        "arrivee_lng": demande.arrivee_lng
-    })
-
-@demande_bp.route('/trajet/<int:id>', methods=['GET'])
-def get_trajet(id):
-    trajet = Trajet.query.get_or_404(id)
-    return jsonify({
-        "id": trajet.id,
-        "point_depart": trajet.point_depart,
-        "point_arrivee": trajet.point_arrivee,
-        "date": trajet.date.isoformat(),
-        "heure_depart": trajet.heure_depart.strftime("%H:%M"),
-        "nb_places": trajet.nb_places,
-        "depart_lat": trajet.depart_lat,
-        "depart_lng": trajet.depart_lng,
-        "arrivee_lat": trajet.arrivee_lat,
-        "arrivee_lng": trajet.arrivee_lng
-    })
