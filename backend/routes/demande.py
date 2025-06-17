8391eb7b292b4c47eb71d10aa507293902e50a0a
Anaiskg <anaiskouagou@gmail.com>
2025-06-17 16:36:28 +0100

    Mise à jour de la route demande

--- backend/routes/demande.py
+++ backend/routes/demande.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from models import db, Demande
+from models import db, Demande, Trajet
 from datetime import datetime
 
 demande_bp = Blueprint('demande', __name__)
@@ -70,4 +70,48 @@ def supprimer_demande(demande_id):
     db.session.commit()
     return jsonify({"message": "Demande supprimée"}), 200
 
+@demande_bp.route('/', methods=['GET'])
+def lister_demandes():
+    demandes = Demande.query.all()
+    return jsonify([{
+        "id": d.id,
+        "point_depart": d.point_depart,
+        "point_arrivee": d.point_arrivee,
+        "date": d.date.isoformat(),
+        "heure_souhaitee": d.heure_souhaitee.strftime("%H:%M"),
+        "depart_lat": d.depart_lat,
+        "depart_lng": d.depart_lng,
+        "arrivee_lat": d.arrivee_lat,
+        "arrivee_lng": d.arrivee_lng
+    } for d in demandes])
 
+@demande_bp.route('/<int:id>', methods=['GET'])
+def get_demande(id):
+    demande = Demande.query.get_or_404(id)
+    return jsonify({
+        "id": demande.id,
+        "point_depart": demande.point_depart,
+        "point_arrivee": demande.point_arrivee,
+        "date": demande.date.isoformat(),
+        "heure_souhaitee": demande.heure_souhaitee.strftime("%H:%M"),
+        "depart_lat": demande.depart_lat,
+        "depart_lng": demande.depart_lng,
+        "arrivee_lat": demande.arrivee_lat,
+        "arrivee_lng": demande.arrivee_lng
+    })
+
+@demande_bp.route('/trajet/<int:id>', methods=['GET'])
+def get_trajet(id):
+    trajet = Trajet.query.get_or_404(id)
+    return jsonify({
+        "id": trajet.id,
+        "point_depart": trajet.point_depart,
+        "point_arrivee": trajet.point_arrivee,
+        "date": trajet.date.isoformat(),
+        "heure_depart": trajet.heure_depart.strftime("%H:%M"),
+        "nb_places": trajet.nb_places,
+        "depart_lat": trajet.depart_lat,
+        "depart_lng": trajet.depart_lng,
+        "arrivee_lat": trajet.arrivee_lat,
+        "arrivee_lng": trajet.arrivee_lng
+    })
